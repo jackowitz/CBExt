88d3b243d44a82f1d921aca7da62bba4537dc73a
Danny Jackowitz <daniel.jackowitz@yale.edu>
2015-04-16 16:45:21 -0400

    Clean up Facebook app auto-authorization.

--- authorize.js
+++ authorize.js
@@ -1,13 +1,11 @@
 // A very fragile, screen-scraping approach to automating
 // authorizing Facebook apps. Just looks all buttons with
 // a particular name and blindly clicks them.
-window.addEventListener('load', function() {
-	var timer = setInterval(buttonReady, 111);
+$(function authorize() {
+	var timer = setInterval(buttonReady, 1111);
 	function buttonReady() {
-		bs = document.getElementsByName('__CONFIRM__')
-		for (var i = 0; i < bs.length; i++) {
-			bs[i].click();
-			clearInterval(timer);
-		}
+		$("[name='__CONFIRM__']").each(function() {
+			$(this).click();
+		});
 	}
-}, false);
+});
